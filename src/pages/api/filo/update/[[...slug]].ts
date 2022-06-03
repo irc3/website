5607c083978fe0d18ab9e460071dd700d7111609
banyudu <banyudu@gmail.com>
2022-06-03 11:51:45 +0800

    feat: support dynamic update from github releases

--- src/pages/api/filo/update/[[...slug]].ts
+++ src/pages/api/filo/update/[[...slug]].ts
@@ -1,12 +1,47 @@
+import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    url: 'https://github.com/irc3/filo/releases/download/v0.1.1/filo.app.tar.gz',
-    version: 'v0.1.1',
-    notes: 'Theses are some release notes',
-    pub_date: '2022-05-25T12:29:53+01:00',
-    signature:
-      'dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVSSlRLYUdRNSs4dk9ISWZ6aUJwSGY5V054c29LOUp0T3FNSUhCUHhIVXNUWjBPZCtPcjZ5Mk9ISHRRZWJZSXRPZUhiTEtOTThCRHBSS1hsSHZMc21BVjNoZUFBaUJkQWc0PQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNjUzNDYzNzU2CWZpbGU6L1VzZXJzL3J1bm5lci93b3JrL2ZpbG8vZmlsby9wYWNrYWdlcy9maWxvL3NyYy10YXVyaS90YXJnZXQvcmVsZWFzZS9idW5kbGUvbWFjb3MvZmlsby5hcHAudGFyLmd6ClVBbnpsMHVCMVdxb0RLRm5NSk1iK3EzNCs1ZzZ3Z0JtcmlyZE13RzBHOGU1T2UzYTFWbzMxU0dXYXo0UkRqM0Y5NGJyMjQ1QzkrSEVyc0JPRExuNkN3PT0K',
-  });
+const platformSuffixMap: Record<string, string> = {
+  darwin: '.app.tar.gz',
+  linux: '.AppImage.tar.gz',
+  win64: '.msi.zip',
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { slug } = req.query;
+  const [platform, version] = (slug as string[]) ?? [];
+  console.log('old version is: ', version);
+
+  const suffix = platformSuffixMap[platform ?? ''];
+  if (suffix) {
+    const releases = await axios.get(
+      'https://api.github.com/repos/irc3/filo/releases'
+    );
+    const latestRelease = releases.data.find(
+      (item: any) => !item.draft && !item.prerelease
+    );
+    if (latestRelease) {
+      const binaryAsset = latestRelease.assets.find((e: any) =>
+        e.name.endsWith(suffix)
+      );
+      const sigAsset = latestRelease.assets.find((e: any) =>
+        e.name.endsWith(`${suffix}.sig`)
+      );
+      if (binaryAsset && sigAsset) {
+        const sigContent = await axios.get(sigAsset.browser_download_url);
+        res.status(200).json({
+          url: binaryAsset.browser_download_url,
+          version: latestRelease.tag_name,
+          notes: latestRelease.body,
+          pub_date: latestRelease.published_at,
+          signature: sigContent.data,
+        });
+        return;
+      }
+    }
+  }
+  res.status(204).end();
 }
